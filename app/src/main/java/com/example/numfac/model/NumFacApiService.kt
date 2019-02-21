ac9f7e112b66b954c6ec6d28546649da8c315d6b
Vova Bezrukov <bezrukovq@yandex.ru>
2019-02-21 15:25:26 +0300

    fixes for detekt

--- app/src/main/java/com/example/numfac/model/NumFacApiService.kt
+++ app/src/main/java/com/example/numfac/model/NumFacApiService.kt
@@ -1,3 +1,12 @@
 package com.example.numfac.model
 
-open class NumFacApiService(){}
+import com.example.numfac.entity.Date
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface NumFacApiService{
+    @GET("{num1}..{num2}/data?json")
+    fun search(@Path("num1") num1: Int,
+               @Path("num2") num2: Int): Observable<List<Date>>
+}
--- app/src/main/java/com/example/numfac/model/RetrofitFactory.kt
+++ app/src/main/java/com/example/numfac/model/RetrofitFactory.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-open class RetrofitFactory(){
+open class RetrofitFactory{
     fun create(): NumFacApiService {
         val retrofit = Retrofit.Builder()
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
