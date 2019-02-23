32e9b677f0283ebfba218064fe7564831cf44175
bezrukovq <38625037+bezrukovq@users.noreply.github.com>
2019-02-23 16:53:07 +0300

    Update RecyclerFragment.kt

--- app/src/main/java/com/example/numfac/view/fragments/RecyclerFragment.kt
+++ app/src/main/java/com/example/numfac/view/fragments/RecyclerFragment.kt
@@ -24,9 +24,9 @@ class RecyclerFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_recycler, container, false)
-    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.fragment_recycler, container, false)
+    
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
