7b33f245804585792f1dad1edf2564375091e61f
KirillKolyshkin <40914279+KirillKolyshkin@users.noreply.github.com>
2019-02-23 18:41:11 +0300

    ф

--- app/src/main/java/com/example/numfac/view/MainActivity.kt
+++ app/src/main/java/com/example/numfac/view/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.numfac.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.numfac.R
-import com.example.numfac.view.fragments.RecyclerFragment
 
 class MainActivity : AppCompatActivity(){
 
