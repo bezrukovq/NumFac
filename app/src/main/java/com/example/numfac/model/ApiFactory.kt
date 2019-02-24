b7832079180ac5e85c42f8bbd51dc4235f89d408
KirillKolyshkin <40914279+KirillKolyshkin@users.noreply.github.com>
2019-02-25 00:05:42 +0300

    last fix

--- app/src/main/java/com/example/numfac/model/ApiFactory.kt
+++ app/src/main/java/com/example/numfac/model/ApiFactory.kt
@@ -4,16 +4,16 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-class ApiFactory {
-    companion object {
-        fun createApi(): NumFacApiService {
-            val retrofit = Retrofit.Builder()
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl(URL)
-                .build()
-            return retrofit.create(NumFacApiService::class.java);
-        }
-        private const val URL = "http://numbersapi.com/"
+object ApiFactory {
+
+    private const val URL = "http://numbersapi.com/"
+
+    fun createApi(): NumFacApiService {
+        val retrofit = Retrofit.Builder()
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(URL)
+            .build()
+        return retrofit.create(NumFacApiService::class.java)
     }
 }
