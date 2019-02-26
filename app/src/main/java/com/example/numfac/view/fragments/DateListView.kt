76f53783051a84d7d55d13f539eeb4dfe02ecacf
Vova Bezrukov <bezrukovq@yandex.ru>
2019-02-26 23:23:13 +0300

    massive work done to make it work

--- app/src/main/java/com/example/numfac/view/fragments/DateListView.kt
+++ app/src/main/java/com/example/numfac/view/fragments/DateListView.kt
@@ -4,7 +4,7 @@ import com.arellomobile.mvp.MvpView
 import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 
-@StateStrategyType(AddToEndSingleStrategy)
+@StateStrategyType(value = AddToEndSingleStrategy::class)
  interface DateListView: MvpView {
     fun showDateList(dataList: List<Int>)
 }
--- app/src/main/java/com/example/numfac/view/fragments/RecyclerFragment.kt
+++ app/src/main/java/com/example/numfac/view/fragments/RecyclerFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.arellomobile.mvp.MvpAppCompatFragment
 import com.example.numfac.R
