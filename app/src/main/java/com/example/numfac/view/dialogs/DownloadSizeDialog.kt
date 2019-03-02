5602ec307a1a978ad11a76f6db02788ae0ebb02d
KirillKolyshkin <40914279+KirillKolyshkin@users.noreply.github.com>
2019-03-02 14:16:52 +0300

    delete extra imports

--- app/src/main/java/com/example/numfac/view/dialogs/DownloadSizeDialog.kt
+++ app/src/main/java/com/example/numfac/view/dialogs/DownloadSizeDialog.kt
@@ -2,13 +2,11 @@ package com.example.numfac.view.dialogs
 
 import android.content.DialogInterface
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.numfac.R
 import com.example.numfac.view.PaginationPreferences
--- app/src/main/java/com/example/numfac/view/fragments/RecyclerFragment.kt
+++ app/src/main/java/com/example/numfac/view/fragments/RecyclerFragment.kt
@@ -1,13 +1,10 @@
 package com.example.numfac.view.fragments
 
 import android.annotation.SuppressLint
-import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.arellomobile.mvp.MvpAppCompatFragment
 import com.arellomobile.mvp.presenter.InjectPresenter
