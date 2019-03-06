535d9c8c381b2c5d4e707e4987b9c9bda6552973
bezrukovq <38625037+bezrukovq@users.noreply.github.com>
2019-03-06 11:10:20 +0300

    Update DateListView.kt

--- app/src/main/java/com/example/numfac/view/fragments/dateList/DateListView.kt
+++ app/src/main/java/com/example/numfac/view/fragments/dateList/DateListView.kt
@@ -1,11 +1,11 @@
 package com.example.numfac.view.fragments.dateList
 
 import com.arellomobile.mvp.MvpView
-import com.arellomobile.mvp.viewstate.strategy.AddToEndStrategy
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
 import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 
-@StateStrategyType(value = AddToEndStrategy::class)
+@StateStrategyType(value = AddToEndSingleStrategy::class)
 interface DateListView : MvpView {
     //@StateStrategyType(value = SkipStrategy::class)
     fun showDateList(dataList: ArrayList<Int>)
