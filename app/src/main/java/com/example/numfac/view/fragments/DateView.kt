234c36c4a4e5613422e8a35993428cfa58f2db20
bezrukovq <38625037+bezrukovq@users.noreply.github.com>
2019-03-06 11:09:35 +0300

    Update DateView.kt

--- app/src/main/java/com/example/numfac/view/fragments/DateView.kt
+++ app/src/main/java/com/example/numfac/view/fragments/DateView.kt
@@ -1,12 +1,12 @@
 package com.example.numfac.view.fragments
 
 import com.arellomobile.mvp.MvpView
-import com.arellomobile.mvp.viewstate.strategy.AddToEndStrategy
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
 import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import com.example.numfac.entity.Date
 
-@StateStrategyType(value = AddToEndStrategy::class)
+@StateStrategyType(value = AddToEndSingleStrategy::class)
 interface DateView: MvpView {
     fun showDate(date: Date)
     fun showMonth(date: Date)
