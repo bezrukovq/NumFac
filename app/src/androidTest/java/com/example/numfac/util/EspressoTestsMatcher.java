92516fc831ead19368051e54bb3f501fac5e6d7f
bezrukovq <bezrukovq@yandex.ru>
2019-04-19 19:07:46 +0300

    UI Tests

---
+++ app/src/androidTest/java/com/example/numfac/util/EspressoTestsMatcher.java
@@ -0,0 +1,21 @@
+package com.example.numfac.util;
+
+import android.view.View;
+
+import org.hamcrest.Matcher;
+
+public class EspressoTestsMatcher {
+
+    public static Matcher<View> withDrawable(final int resourceId) {
+        return new DrawableMatcher(resourceId);
+    }
+
+    public static Matcher<View> noDrawable() {
+        return new DrawableMatcher(DrawableMatcher.EMPTY);
+    }
+
+    public static Matcher<View> hasDrawable() {
+        return new DrawableMatcher(DrawableMatcher.ANY);
+    }
+
+}
